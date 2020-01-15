b91795b4cccdefc7d85a3977728fa02fa6893acc
dan.nicolae <micdan1994@gmail.com>
2020-01-15 22:43:02 +0200

    Added search by post code and also by street.

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Cartiere;
+use App\Models\Strazi;
 use Illuminate\Http\Request;
 
 class SearchController extends Controller
@@ -24,10 +24,27 @@ class SearchController extends Controller
      */
     public function autocomplete(Request $request)
     {
-        $data = Cartiere::join('strazi', 'strazi.id_cartier', '=', 'cartiere.id')
-            ->select('strazi.*', 'cartiere.*')
-            ->where("strazi.nume_strada","LIKE","%{$request->search}%")
-            ->get();
+        $attribute_chosen = $request->by_attribute;
+
+        if ($attribute_chosen === 'post_code_search') {
+            $data = Strazi::join('cartiere', 'strazi.id_cartier', '=', 'cartiere.id')
+                ->join('cladiri', 'cladiri.id_strada', '=', 'strazi.id')
+                ->join('tip_strada', 'strazi.id_tip_strada', '=', 'tip_strada.id')
+                ->join('tip_cladire', 'cladiri.id_tip_cladire', '=', 'tip_cladire.id')
+                ->select('cartiere.*', 'strazi.*', 'cladiri.*', 'tip_strada.nume as tip_strada_nume', 'tip_cladire.nume as tip_cladire_nume')
+                ->where("cladiri.cod_postal", "LIKE", "%{$request->search}%")
+                ->orderBy('strazi.nume_strada', 'ASC')
+                ->get();
+        } else {
+            $data = Strazi::join('cartiere', 'strazi.id_cartier', '=', 'cartiere.id')
+                ->join('cladiri', 'cladiri.id_strada', '=', 'strazi.id')
+                ->join('tip_strada', 'strazi.id_tip_strada', '=', 'tip_strada.id')
+                ->join('tip_cladire', 'cladiri.id_tip_cladire', '=', 'tip_cladire.id')
+                ->select('cartiere.*', 'strazi.*', 'cladiri.*', 'tip_strada.nume as tip_strada_nume', 'tip_cladire.nume as tip_cladire_nume')
+                ->where("strazi.nume_strada", "LIKE", "%{$request->search}%")
+                ->orderBy('strazi.nume_strada', 'ASC')
+                ->get();
+        }
 
         return response()->json($data);
     }
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -9,16 +9,63 @@
 <body>
 
 <div class="container">
-    <h1>Cautare Baza De Date Strazi si altele</h1>
-    <input class="typeahead form-control" type="text">
+    <section >
+        <h1>Cautare Baza De Date Strazi si altele</h1>
+        <div class="col-md-12">
+            <label for="street_search">Cautare Strada</label>
+            <input id="street_search" class="typeahead form-control" type="text">
+            <label for="post_code_search">Cautare dupa Cod Postal</label>
+            <input id="post_code_search" class="typeahead form-control" type="text">
+        </div>
+    </section>
+    <br/>
+    <br/>
+    <br/>
+</div>
+
+<div class="row offset-md-1 col-md-10">
+        <table id="evidenta" class="table table-striped table-dark">
+            <thead>
+            <tr>
+                <th scope="col">Cartier</th>
+                <th scope="col">Strada</th>
+                <th scope="col">Tip Strada</th>
+                <th scope="col">Total Cladiri</th>
+                <th scope="col">Numar Cladire</th>
+                <th scope="col">Cod-Postal</th>
+                <th scope="col">Tip Cladire</th>
+                <th scope="col">Numar Apartamente</th>
+            </tr>
+            </thead>
+            <tbody>
+            </tbody>
+        </table>
 </div>
 
 <script type="text/javascript">
     var path = "{{ route('autocomplete') }}";
     $('input.typeahead').typeahead({
-        source:  function (query, process) {
-            return $.get(path, { search: query }, function (data) {
-                return process(data);
+        source:  function (query) {
+            const searchedBy = this.$element.attr('id');
+            debugger
+            return $.get(path, { search: query, by_attribute: searchedBy }, function (data) {
+                let resultData = data;
+                $("#evidenta > tbody > tr").remove();
+                for(let key in resultData){
+                    let nrAps = resultData[key]['nr_apartamente'] === 1 ? 'N/A' : resultData[key]['nr_apartamente'];
+                    $('#evidenta> tbody:last-child').append(
+                        "<tr>" +
+                            "<th scope=\"row\">" + resultData[key]['nume_cartier']     + "</th>" +
+                            "<td>" + resultData[key]['nume_strada']     + "</td>" +
+                            "<td>" + resultData[key]['tip_strada_nume']     + "</td>" +
+                            "<td>" + resultData[key]['nr_cladiri']     + "</td>" +
+                            "<td>" + resultData[key]['numar_cladire']     + "</td>" +
+                            "<td>" + resultData[key]['cod_postal']     + "</td>" +
+                            "<td>" + resultData[key]['tip_cladire_nume']     + "</td>" +
+                            "<td>" + nrAps     + "</td>" +
+                        "</tr>"
+                    );
+                }
             });
         }
     });
